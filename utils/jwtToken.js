67ba7e39139fe89487ef1b6220ec7e358ee1dc6e
YHJ96 <yhj878812@naver.com>
2022-08-27 21:05:33 +0900

    refactor: jwt 모듈화 완료

---
+++ utils/jwtToken.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken');
+
+const PRIVATE_KEY = 'SEB_39_38';
+const option = { algorithm: 'HS256', expiresIn: '30m' };
+
+const jwtSign = async (payload) => {
+  const access_token = jwt.sign({ id: payload }, PRIVATE_KEY, option);
+  const refresh_token = jwt.sign({ id: payload }, PRIVATE_KEY, {
+    ...option,
+    expiresIn: '14d',
+  });
+  return { access_token, refresh_token };
+};
+
+const jwtVerify = async (access_token, refresh_token) => {
+  const typeNumber = ['type1', 'type2', 'type3'];
+  const isCheck = {
+    access: await jwtAccessVerify(access_token),
+    refresh: await jwtRefreshVerify(refresh_token),
+  };
+  if (isCheck.access) return typeNumber[0];
+  if (isCheck.access === null && isCheck.refresh) return typeNumber[1];
+  if (!(isCheck.access && isCheck.refresh)) return typeNumber[2];
+};
+
+const jwtAccessVerify = async (jwtToken) => {
+  try {
+    const decodeJWT = jwt.verify(jwtToken, PRIVATE_KEY);
+    return decodeJWT;
+  } catch {
+    return null;
+  }
+};
+
+const jwtRefreshVerify = async (jwtToken) => {
+  try {
+    const decodeJWT = jwt.verify(jwtToken, PRIVATE_KEY);
+    return decodeJWT;
+  } catch {
+    return null;
+  }
+};
+
+module.exports = {
+  jwtSign,
+  jwtVerify,
+  jwtRefreshVerify,
+};
