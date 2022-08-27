b9380be4295dbfe4c90fd287524ce438a3708bef
YHJ96 <yhj878812@naver.com>
2022-08-27 21:08:28 +0900

    feat: userCheckMiddleWare 구현 완료

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 const PORT = 8080;
@@ -8,10 +9,30 @@ const PORT = 8080;
 const userRouter = require('./routers/user');
 const answerRouter = require('./routers/answer');
 const questionRouter = require('./routers/question');
+const { jwtSign, jwtVerify, jwtRefreshVerify } = require('./utils/jwtToken');
 
+const userCheckMiddleWare = async (req, res, next) => {
+  const { originalUrl, method } = req;
+  if (originalUrl === '/user' && method === 'POST') return next();
+  const { access_token, refresh_token } = req.cookies;
+  const type = await jwtVerify(access_token, refresh_token);
+  const token = await jwtRefreshVerify(refresh_token);
+  console.log(type, token);
+  if (type === 'type1') return next();
+  if (type === 'type2') {
+    const { access_token } = await jwtSign(token.id);
+    res.cookie('access_token', access_token, { httpOnly: true });
+  }
+  if (type === 'type3') return res.send('로그인을 다시 해주세요.');
+  return res.send('타입이 없습니다.');
+};
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(morgan('tiny'));
+app.use(userCheckMiddleWare);
 
 app.use('/user', userRouter);
 app.use('/answer', answerRouter);
