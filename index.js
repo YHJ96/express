32cac9ee5ecf09e13eb31c79e39b135783f5c7f8
YHJ96 <yhj878812@naver.com>
2022-08-28 22:16:39 +0900

    feat: dotenv 설정 완료

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
+require('dotenv').config();
 
 const PORT = 8080;
 
